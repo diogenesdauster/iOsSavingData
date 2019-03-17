66a3382eab34222ab6cd2edadac7af6a80f0b7d9
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-03-17 20:26:22 -0300

    feat: working with JSON using decode

--- iOsSaveData.playground/Pages/JSON.xcplaygroundpage/Contents.swift
+++ iOsSaveData.playground/Pages/JSON.xcplaygroundpage/Contents.swift
@@ -1,7 +1,36 @@
 //: [Previous](@previous)
 
-import Foundation
+import UIKit
 
-var str = "Hello, playground"
+struct Image: Decodable {
+    enum Kind: String, Decodable {
+        case scene
+        case sticker
+    }
+    
+    enum DecodingError: Error {
+        case missingFile
+    }
+    
+    let name: String
+    let kind: Kind
+    let pngData: Data
+    
+}
+
+extension Array where Element == Image {
+    init(filename: String) throws {
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "json") else {
+            throw Image.DecodingError.missingFile
+        }
+        let decoder = JSONDecoder()
+        let data = try Data(contentsOf: url)
+        self = try decoder.decode([Image].self, from: data)
+    }
+}
+
+
+let images = try [Image](filename: "images")
+images.map{ UIImage(data: $0.pngData) }
 
 //: [Next](@next)
