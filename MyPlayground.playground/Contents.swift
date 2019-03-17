7d2c51c00bcef952ff8fcd4a5528867e8becfc42
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-03-17 17:39:42 -0300

    feat: create some data with FileManager

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,12 @@
 import Foundation
 
+let mysteryDataURL = URL(fileURLWithPath: "mystery", relativeTo: FileManager.documentDirectoryURL)
 
+let stringURL = FileManager.documentDirectoryURL
+.appendingPathComponent("string")
+.appendingPathExtension("txt")
 
-FileManager.documentDirectoryURL
+stringURL.path
 
 
 
