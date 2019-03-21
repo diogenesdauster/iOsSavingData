961878e0b8941659a7e6ace7a36d8bd27066a2e1
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-03-20 21:50:18 -0300

    refactor: decode and encode array in JSON fileß

--- iOsSaveData.playground/Pages/Codable Type.xcplaygroundpage/Contents.swift
+++ iOsSaveData.playground/Pages/Codable Type.xcplaygroundpage/Contents.swift
@@ -11,19 +11,24 @@ let sticker = Sticker(name: "Diógenes Dauster",birthday: DateComponents(calenda
     normalizedPosition: CGPoint(x: 0.3, y: 0.5),
     imageName: "cat")
 
+let anotherSticker = Sticker(name: "Ana Beatriz", birthday: DateComponents(calendar: .current, year: 1992, month: 12, day: 23).date!,
+                             normalizedPosition: CGPoint(x: 0.3, y: 0.5),
+                             imageName: "dog")
 do {
-  let jsonURL = URL(fileURLWithPath: sticker.name, relativeTo: FileManager.documentDirectoryURL.appendingPathComponent(Image.Kind.sticker.rawValue)).appendingPathExtension("json")
+//  let jsonURL = URL(fileURLWithPath: sticker.name, relativeTo: FileManager.documentDirectoryURL.appendingPathComponent(Image.Kind.sticker.rawValue)).appendingPathExtension("json")
+  let jsonURL = URL(fileURLWithPath: "stickers", relativeTo: FileManager.documentDirectoryURL.appendingPathComponent(Image.Kind.sticker.rawValue)).appendingPathExtension("json")
+
     
   let jsonEncoder = JSONEncoder()
     jsonEncoder.outputFormatting = .prettyPrinted
-    let jsonData = try jsonEncoder.encode(sticker)
+    let jsonData = try jsonEncoder.encode([sticker,anotherSticker])
     try jsonData.write(to: jsonURL, options: .atomic)
     
     let jsonDecoder = JSONDecoder()
     let savedJSONData = try Data(contentsOf: jsonURL)
-    let jsonSticker = try jsonDecoder.decode(Sticker.self, from: savedJSONData)
+    let jsonStickers = try jsonDecoder.decode([Sticker].self, from: savedJSONData)
     
-    jsonSticker == sticker
+    jsonStickers
     
     
 } catch { print(error) }
